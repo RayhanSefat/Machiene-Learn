19e0486b1861a93df30bfd78a6ddbe180d0dc9df
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-02-28 19:44:24 +0600

    joblib model created

--- Single variable Regression/Linear Regression.py
+++ Single variable Regression/Linear Regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import plotly.express as px
 import pickle
+import joblib
 import os
 default_directory = "E:\RAYHAN SEFAT\Machiene Learn"
 os.chdir(default_directory)
@@ -46,3 +47,5 @@ plt.show()
 
 with open('Single variable Regression/pickle_model', 'wb') as f:
     pickle.dump(reg, f)
+
+joblib.dump(reg, 'Single variable Regression/joblib_model')
