a77409e69a49b8fa3f074947e2baaed0b3768429
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-02-28 19:49:46 +0600

    tested my joblib model

--- Single variable Regression/tester.py
+++ Single variable Regression/tester.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import plotly.express as px
 import pickle
+import joblib
 import os
 default_directory = "E:\RAYHAN SEFAT\Machiene Learn"
 os.chdir(default_directory)
 
 with open('Single variable Regression/pickle_model', 'rb') as f:
-    md = pickle.load(f)
-    print(md.predict([[3450]]))
+    mp = pickle.load(f)
+print("valule from pickle model {}".format(mp.predict([[3450]])))
+
+mj = joblib.load('Single variable Regression/joblib_model')
+print("valule from joblib model {}".format(mj.predict([[3450]])))
